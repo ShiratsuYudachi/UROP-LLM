b3fdbf5c45e3bd1103e6209bb69d5dda54b69095
ShiratsuYudachi <shiratsuyudachi@gmail.com>
2024-02-29 19:56:27 +0800

    fetch test case, myparser

--- CodeCollector/FetchManager.py
+++ CodeCollector/FetchManager.py
@@ -31,56 +31,97 @@ def parse_submission_html(tr):
         verdict = Verdict.parse_dict(verdict_str)
         return CFSubmission(contestID="1915", problem=problem, submissionID=submission_id, author=author, lang=lang, verdict=verdict, runTime=run_time, memory=memory, submissionTime=submission_time)
 
+def myparser():
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    if response.status_code == 200:
+        submissions = []
+        table_items = soup.find('datatable').find('table').find_all('td')
+        def mystrip(item):
+            return item.strip().replace('\n','').replace('\r','').replace(' ','')
+        submission_id = mystrip(table_items[0].text)
+        author = mystrip(table_items[1].text)
+        problem = mystrip(table_items[2].text)
+        lang = table_items[3].text.strip()
+        verdict_str = table_items[4].text.strip().replace('\n','').replace('\r','')
+        runTime = int(mystrip(table_items[5].text).strip('ms'))
+        memory = int(mystrip(table_items[6].text).strip('KB'))
+        time_str = table_items[7].text.strip()
+        submission_time = datetime.strptime(time_str, '%Y-%m-%d %H:%M:%S').timestamp()
 
-def fetch_submission_code(contest_id, submission_id):
+        isAccepted = verdict_str=='Accepted'
+        errorType = None
+        errorCaseNo = 0
+        if not isAccepted:
+            if 'Compilation' in verdict_str:
+                errorType = 'Compilation'
+            elif 'Runtime' in verdict_str:
+                errorType = 'Runtime'
+            elif 'Time' in verdict_str:
+                errorType = 'Time'
+            elif 'Wrong' in verdict_str:
+                errorType = 'Answer'
+            else:
+                errorType = verdict_str
+            if errorType in ('Compilation','Runtime','Time'):
+                errorCaseNo = verdict_str.split(' ')[-1]
+    
+# fetch submission code and failed test case
+def fetch_submission_code_error(contest_id, submission_id, errorCaseNo = None):
     # Build the URL from the given contest_id and submission_id
     url = f"https://codeforces.com/contest/{contest_id}/submission/{submission_id}"
-
-    # Make a request to the URL
-    response = requests.get(url)
+    session = requests.Session()
+    response = session.get(url)
     
-    # Parse the HTML content of the page
     soup = BeautifulSoup(response.text, 'html.parser')
     
     # Find the <pre> tag with the specific id containing the submission code
     code_pre_tag = soup.find('pre', id="program-source-text")
     
     # Extract the text from the <pre> tag, which is the submission code
-    code = code_pre_tag.get_text() if code_pre_tag else "Code not found"
     
-    return code
+    code = code_pre_tag.get_text()
+    if not code:
+        colorPrint("ERROR: Code Not Found in submission page")
+        return
 
-# TODO: test below
-def fetch_error_test_case():
-    url = "https://codeforces.com/data/submitSource"
+    errorCase = None
 
-# Headers as specified in the fetch request
-    headers = {
-        "accept": "*/*",
-        "accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,zh-TW;q=0.6",
-        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-        "sec-ch-ua": "\"Not A(Brand\";v=\"99\", \"Google Chrome\";v=\"121\", \"Chromium\";v=\"121\"",
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": "\"macOS\"",
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-origin",
-        "x-csrf-token": "e72c792cf3bcca95c4647f0736be7c2a",
-        "x-requested-with": "XMLHttpRequest"
-    }
+    # find failed test cases
+    if errorCaseNo:
+        meta_tag = soup.find('meta', attrs={'name': 'X-Csrf-Token'})
+        if not meta_tag:
+            colorPrint("ERROR: CSRF token Not Found in submission page")
+            return
+        csrf_token = meta_tag['content']
+        submitSource_url = "https://codeforces.com/data/submitSource"
+        headers = {
+            "accept": "*/*",
+            "accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,zh-TW;q=0.6",
+            "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "sec-ch-ua": "\"Not A(Brand\";v=\"99\", \"Google Chrome\";v=\"121\", \"Chromium\";v=\"121\"",
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": "\"macOS\"",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-origin",
+            "x-csrf-token": csrf_token,
+            "x-requested-with": "XMLHttpRequest",
+            'Referer': url
+        }
+        data = {
+            "submissionId": "248637681",
+            "csrf_token": csrf_token
+        }
+        response = session.post(submitSource_url, headers=headers, data=data)
+        errorCase = {
+            'input': response[f'input#{errorCaseNo}'],
+            'output': response[f'output#{errorCaseNo}'],
+            'expectedOutput': response[f'answer#{errorCaseNo}'],
+            'CheckerComment': response[f'checkerStdoutAndStderr#{errorCaseNo}']
+        }
 
-    # Data to be sent with the request
-    data = {
-        "submissionId": "248602789",
-        "csrf_token": "e72c792cf3bcca95c4647f0736be7c2a"
-    }
+    
+    return code, errorCase
 
-    # Sending the POST request
-    response = requests.post(url, headers=headers, data=data)
-
-    # Assuming the response is in JSON format, convert it to a dictionary
-    result_dict = response.json()
-
-    print(result_dict)
-
-#fetch_error_test_case()
+print(fetch_submission_code_error(1914,248602789,errorCaseNo=0))
